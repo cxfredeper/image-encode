3337e7fe11b0c2f559a2613f81fb092535d50af1
cxfredeper <cxfredeper@gmail.com>
2025-05-08 04:20:15 -0400

    added encoder thingy

---
+++ index.js
@@ -0,0 +1,59 @@
+"use strict";
+
+
+async function generateImage() {
+	let file = document.getElementById("in").files[0];
+
+	if (file.size > 2**32 - 1) {
+		alert(`File too large! (${file.size} bytes)`);
+		return;
+	}
+
+	let payload = new Uint8Array(await file.arrayBuffer());
+	let imgData = encodeImage(payload);
+	setImage(imgData);
+}
+
+
+function setImage(data) {
+	let out = document.getElementById("out");
+	out.src = URL.createObjectURL(new Blob([data]));
+}
+
+
+function writeHeader(buffer, payload) {
+	let l = payload.length;
+	buffer[0] = 0xff & (l >> 24);
+	buffer[1] = 0xff & (l >> 16);
+	buffer[2] = 0xff & (l >> 8);
+	buffer[3] = 0xff & l;
+}
+
+
+function writeBlackPixels(buffer, fromIdx) {
+	for (let i = fromIdx; i < buffer.length; ++i)
+		buffer[i] = 0;
+}
+
+
+function encodeImage(payload) {
+	// ARGB four channels, so each 4 byte sequence is made a pixel,
+	// plus one pixel for the header.
+	let width = Math.ceil(Math.sqrt(1 + payload.length / 4));
+
+	const headerLength = 4;
+	let paddingLength = (width * width) * 4 - headerLength - payload.length;
+
+	console.debug(`header: ${headerLength}`);
+	console.debug(`payload: ${payload.length}`);
+	console.debug(`padding: ${paddingLength}`);
+
+	let data = new Uint8ClampedArray(headerLength + payload.length + paddingLength);
+	console.debug(`package: ${data.length + paddingLength}`);
+
+	writeHeader(data, payload);
+	data.set(payload, headerLength)
+	writeBlackPixels(data, headerLength + payload.length);
+
+	return UPNG.encode([data.buffer], width, width, 0)
+}
